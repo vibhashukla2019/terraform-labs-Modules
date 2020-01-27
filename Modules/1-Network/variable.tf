48af05cbc62c9df1262a5fb9d5f1527818d382ca
Vibha Shukla <Vibha.Shukla@ey.com>
2020-01-27 18:23:39 +0530

    Terraform VM creation

---
+++ Modules/1-Network/variable.tf
@@ -0,0 +1,11 @@
+variable "location" {
+
+description = "Location where to deploy resources"
+
+}
+
+variable "rg_network" {
+
+description = "Name of the Resource Group where resources will be deployed"
+
+}
---
+++ Provider.tf
@@ -0,0 +1,6 @@
+provider "azurerm" {
+
+subscription_id = "a30b93e1-5b15-405c-80f5-f88d07c7356b"
+tenant_id = "415a8c7e-8647-4b46-b291-9cd6c3cde41d"
+
+}
